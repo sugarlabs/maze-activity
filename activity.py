a79312b22f5701fd804b711157ff3ac0bcfb0515
Manuel Quiñones <manuq@laptop.org>
2011-08-31 21:00:07 -0300

    Maze: fix to make activity start again
    The game_name attribute of PyGameActivity seems to be changed
    accidentally from 'game' to 'maze'.  As the docstring of
    PyGameActivity class states, game_name needs to match the name of a
    module that contains the game loop in a function called main.
    
    TIP: please test at least if the activity start before commit :)
    
    Signed-off-by: Manuel Quiñones <manuq@laptop.org>
    Signed-off-by: Rafael Ortiz <rafael@activitycentral.com>

--- activity.py
+++ activity.py
@@ -4,6 +4,6 @@ from gettext import gettext as _
 
 
 class MazeActivity(olpcgames.PyGameActivity):
-    game_name = 'maze'
+    game_name = 'game'
     game_title = _('Maze')
     game_size = None    # let olpcgames pick a nice size for us
