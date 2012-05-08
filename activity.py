87f832850ba242b1606acb0f1d60bc5631920f34
Manuel Kaufmann <humitos@gmail.com>
2012-05-08 12:04:06 -0300

    Maze icon in the toolbar SL #3380
     - Show the Maze icon in the toolbar.
    
     - Don't use olpcgames.PygameActivity.build_toolbar anymore because it
       doesn't do what we need (SL#3474 and SL#3380) .Used our own build_toolbar method instead.
    
    Signed-off-by: Manuel Kaufmann <humitos@gmail.com>
    Signed-off-by: Rafael Ortiz <rafael@activitycentral.com>

--- activity.py
+++ activity.py
@@ -4,6 +4,9 @@ import olpcgames
 import pygame
 import gtk
 
+from sugar.activity.widgets import ActivityToolbarButton
+from sugar.activity.widgets import StopButton
+from sugar.graphics.toolbarbox import ToolbarBox
 from sugar.graphics.toolbutton import ToolButton
 from gettext import gettext as _
 
@@ -15,26 +18,41 @@ class MazeActivity(olpcgames.PyGameActivity):
 
     def build_toolbar(self):
         """Build our Activity toolbar for the Sugar system."""
-        toolbar = super(MazeActivity, self).build_toolbar()
+
+        toolbar_box = ToolbarBox()
+        activity_button = ActivityToolbarButton(self)
+        toolbar_box.toolbar.insert(activity_button, 0)
+        activity_button.show()
 
         separator = gtk.SeparatorToolItem()
-        separator.set_expand(True)
-        separator.set_draw(False)
-        toolbar.insert(separator, 0)
-
-        harder_button = ToolButton('create-harder')
-        harder_button.set_tooltip(_('Harder level'))
-        harder_button.connect('clicked', self._harder_button_cb)
-        toolbar.insert(harder_button, 2)
-        harder_button.show()
+        toolbar_box.toolbar.insert(separator, -1)
+        separator.show()
 
         easier_button = ToolButton('create-easier')
         easier_button.set_tooltip(_('Easier level'))
         easier_button.connect('clicked', self._easier_button_cb)
-        toolbar.insert(easier_button, 2)
-        easier_button.show()
+        toolbar_box.toolbar.insert(easier_button, -1)
 
-        return toolbar
+        harder_button = ToolButton('create-harder')
+        harder_button.set_tooltip(_('Harder level'))
+        harder_button.connect('clicked', self._harder_button_cb)
+        toolbar_box.toolbar.insert(harder_button, -1)
+
+        separator = gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_size_request(0, -1)
+        separator.set_expand(True)
+        toolbar_box.toolbar.insert(separator, -1)
+        separator.show()
+
+        stop_button = StopButton(self)
+        toolbar_box.toolbar.insert(stop_button, -1)
+        stop_button.show()
+
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show_all()
+
+        return toolbar_box
 
     def _easier_button_cb(self, button):
         pygame.event.post(olpcgames.eventwrap.Event(
