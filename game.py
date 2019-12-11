87d37787257d08e3c812d8726c2625614e18ae27
James Cameron <quozl@laptop.org>
2019-12-12 10:09:46 +1100

    Review - better explain unusual solution

--- game.py
+++ game.py
@@ -579,6 +579,9 @@ class MazeGame(Gtk.DrawingArea):
             self.handleMessage(None, message)
             return
 
+        # FIXME: Buddy.props.key is a quoted bytes, but self.my_key is not,
+        # until fixed we shall quote the received key so comparison works
+        # https://github.com/sugarlabs/sugar-toolkit-gtk3/issues/433
         key = str(key.encode())
         if key in self.remoteplayers:
             player = self.remoteplayers[key]
